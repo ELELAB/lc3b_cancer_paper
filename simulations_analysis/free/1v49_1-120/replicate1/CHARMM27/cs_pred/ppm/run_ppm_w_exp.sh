218236b737101e5a184c0d3c96816c2321715159
Elena Papaleo <elena@kb-bioinfo01.cancer.dk>
2020-06-23 14:26:49 +0200

    added cs pred

---
+++ simulations_analysis/free/1v49_1-120/replicate1/CHARMM27/cs_pred/ppm/run_ppm_w_exp.sh
@@ -0,0 +1,2 @@
+#PPM_One need to be downloaded and used for the analysis
+./ppm_linux -pdb ../pdb_movies/movie.dt500.pdb -bmrb ../../../../exp_ref/cs_pred/1v49_BRMB_CS_data.txt  
---
+++ simulations_analysis/free/1v49_1-120/replicate1/RSFF2/cs_pred/ppm/run_ppm_w_exp.sh
@@ -0,0 +1,2 @@
+#PPM_One need to be downloaded and used for the analysis
+./ppm_linux -pdb ../pdb_movies/movie.dt500.pdb -bmrb ../../../additional_files/1v49_BRMB_CS_data.txt  
