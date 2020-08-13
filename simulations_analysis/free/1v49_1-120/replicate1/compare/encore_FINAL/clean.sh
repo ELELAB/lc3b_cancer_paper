ee542f19ff94d57bfd8ffad51e08e6ed61591676
Elena Papaleo <elena@kb-bioinfo01.cancer.dk>
2020-08-13 12:36:24 +0200

    revised encore compare

--- simulations_analysis/free/1v49_1-120/replicate1/compare/encore_FINAL/clean.sh
+++
@@ -1,1 +0,0 @@
-rm -f *.pdf *.dat merged.xtc rmsd.dat rmsd.xvg rmsd.xpm
--- simulations_analysis/free/1v49_1-120/replicate1/compare/encore_FINAL/do_plot.sh
+++
@@ -1,9 +0,0 @@
-labels=$(python -c 'import md; print(" ".join(md.labels), end="")')
-
-# plot ensemble comparison matrices
-../structural_alphabet/plot_jsd ces.dat  -w 1 -l 1 -m 0 -x 1 -s 5 5 -c gray_r -t CES -b $labels -L -o ces -N -B e -C 2 -x 1.0
-../structural_alphabet/plot_jsd dres.dat -w 1 -l 1 -m 0 -x 1 -s 5 5 -c gray_r -t DRES -b $labels -L -o dres -N -B e -C 2 -x 1.0
-python plot_projection.py dres_projection.dat
-python plot_space.py space_ces.dat
-python plot_space.py space_dres.dat
-
