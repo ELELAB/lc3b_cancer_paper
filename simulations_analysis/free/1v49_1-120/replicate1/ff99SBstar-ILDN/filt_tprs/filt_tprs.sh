ac68992481769a7890b4e80cb45c62820cf00ad6
Elena Papaleo <elena@kb-bioinfo01.cancer.dk>
2020-08-13 12:55:40 +0200

    ff99SBstar-ILDN pca

---
+++ simulations_analysis/free/1v49_1-120/replicate1/ff99SBstar-ILDN/filt_tprs/filt_tprs.sh
@@ -0,0 +1,13 @@
+tpr=md
+#tpr filtered on  protein
+gmx convert-tpr -s ../../../../../../../simulations/lc3b/free/1v49_1-120/replicate1/ff99SBstar-ILDN/md/$tpr.tpr -n ../index_files/protein.ndx -o md_prot.tpr
+#tpr filtered  with no Cterm and Nterm all atoms
+gmx convert-tpr -s ../../../../../../../simulations/lc3b/free/1v49_1-120/replicate1/ff99SBstar-ILDN/md/$tpr.tpr -n ../index_files/AAnoCNter.ndx -o AAnoCNter.tpr << eof
+1
+eof
+#tpr filtered with no Cterm and Nterm - for calpha only
+gmx convert-tpr -s ../../../../../../../simulations/lc3b/free/1v49_1-120/replicate1/ff99SBstar-ILDN/md/$tpr.tpr -n ../index_files/noCNter.ndx -o noCNter.tpr << eof
+1
+eof
+#tpr filtered the calpha atom 
+gmx convert-tpr -s ../../../../../../../simulations/lc3b/free/1v49_1-120/replicate1/ff99SBstar-ILDN/md/$tpr.tpr -n ../index_files/ca.ndx -o  md_ca.tpr
